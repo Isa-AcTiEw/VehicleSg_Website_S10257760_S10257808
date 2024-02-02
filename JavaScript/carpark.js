51b928cce78cad8582831fb54950bd268e94b363
Isa-AcTiEw <s10257760@gmail.com>
2024-02-02 22:37:25 +0800

    New changes made

--- JavaScript/carpark.js
+++ JavaScript/carpark.js
@@ -31,44 +31,70 @@ searchButton.addEventListener('click', function(event) {
 });
 
 
-var carparkObj;
-// send HTTP request to the API using the user's input
-function processData(inputValue,carparkObj){
-    // GET METHOD to return an array of javascript objects
-    var myHeaders = new Headers();
-    myHeaders.append("AccountKey", "jRHACKiESaGsYNOFNdxSKw==");
+// var carparkObj;
+// // send HTTP request to the API using the user's input
+// function processData(inputValue,carparkObj){
+//     // GET METHOD to return an array of javascript objects
+//     var myHeaders = new Headers();
+//     myHeaders.append("AccountKey", "jRHACKiESaGsYNOFNdxSKw==");
     
-    var requestOptions = {
-        method: 'GET',
-        headers: myHeaders,
-        redirect: 'follow'
-        };
+//     var requestOptions = {
+//         method: 'GET',
+//         headers: myHeaders,
+//         redirect: 'follow'
+//         };
 
-    fetch("http://datamall2.mytransport.sg/ltaodataservice/CarParkAvailabilityv2", requestOptions)
-    .then(response => {
-    if (!response.ok) {
-    throw new Error('Network response was not ok');
-    }
-    return response.text();
-    })
-    .then(result => {
-    console.log(result);
-    // Continue processing the result as needed
-    })
-    .catch(error => {
-    console.log('Error:', error);
-    // Handle errors here
-    });
+//     fetch("http://datamall2.mytransport.sg/ltaodataservice/CarParkAvailabilityv2", requestOptions)
+//     .then(response => {
+//     if (!response.ok) {
+//     throw new Error('Network response was not ok');
+//     }
+//     return response.text();
+//     })
+//     .then(result => {
+//     console.log(result);
+//     // Continue processing the result as needed
+//     })
+//     .catch(error => {
+//     console.log('Error:', error);
+//     // Handle errors here
+//     });
    
 
 
     // check if userinput is inside the array address or 
-}
 
-processData(inputValue,carparkObj)
+
+
 
 var myHeaders = new Headers();
 myHeaders.append("AccountKey", "jRHACKiESaGsYNOFNdxSKw==");
 
+// call carpark availability api
 
+// retrieve all the carpark number with their corresponding lots available and store in js object
 
+function retrievedata(){
+    fetch("DataFiles/HDBCarparkInformation.csv")
+    .then(response => response.text())
+    .then(data => {
+        const lines = data.split('\n')
+        const carparkinfo = lines.splice(1);
+        carparkinfo.forEach(element =>{
+            
+        }
+
+        )
+    })
+    .catch(error => console.error('Error:', error));
+    
+}
+    
+
+retrievedata()
+
+  
+  fetch("https://api.data.gov.sg/v1/transport/carpark-availability")
+    .then(response => response.text())
+    .then(result => console.log(result))
+    .catch(error => console.log('error', error));
