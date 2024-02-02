b1b91c7efd3d9abbb476e857d66b7146e615a914
Isa-AcTiEw <s10257760@gmail.com>
2024-02-02 16:20:46 +0800

    Added the logic for traffic updates

--- JavaScript/traffic.js
+++ JavaScript/traffic.js
@@ -7,7 +7,58 @@ var requestOptions = {
   redirect: 'follow'
 };
 
+
+var trafficdoc = document.getElementsByClassName('traffic-updates')
+console.log(trafficdoc)
+var trafficObj;
 fetch("http://datamall2.mytransport.sg/ltaodataservice/TrafficIncidents", requestOptions)
-  .then(response => response.text())
-  .then(result => console.log(result))
-  .catch(error => console.log('error', error));
+  .then(response => response.json())
+  .then (data => {
+    console.log(data);
+    traffic = data
+  });
+  
+trafficObj.array.forEach(element => {
+    const container = document.createElement('div')
+    container.ClassName = 'traff-container'
+    if(element.Type = "RoadWork"){
+        image = document.createElement('img')
+        paratext = document.createElement('p')
+        paratext.textContent = element.Message
+        container.appendChild(image)
+        container.appendChild(paratext)
+        trafficdoc.appendChild(container)
+
+
+    }
+
+    else if(element.Type == "Vehicle Breakdown"){
+        image = document.createElement('img')
+        paratext = document.createElement('p')
+        paratext.textContent = element.Message
+        container.appendChild(image)
+        container.appendChild(paratext)
+        trafficdoc.appendChild(container)
+    }
+
+    else if(element.Type == "Accident"){
+        image = document.createElement('img')
+        paratext = document.createElement('p')
+        paratext.textContent = element.Message
+        container.appendChild(image)
+        container.appendChild(paratext)
+        trafficdoc.appendChild(container)
+    }
+
+    else{
+        heading = document.createElement(h1)
+        heading.textContent = element.Type
+        updatespara = document.createElement('p')
+        updatespara.textContent = element.Message
+        container.appendChild(heading)
+        container.appendChild(updatespara)
+        trafficdoc.appendChild(container)
+    }
+});
+  
+  
