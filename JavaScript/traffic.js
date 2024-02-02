75056cb650d95f724ab1688d81022cdc3bc87f7f
Isa-AcTiEw <s10257760@gmail.com>
2024-02-02 14:50:35 +0800

    New datafile for api testing

--- JavaScript/traffic.js
+++ JavaScript/traffic.js
@@ -1,21 +1,13 @@
-var api_key = "jRHACKiESaGsYNOFNdxSKw==";
-var apiurl = "http://datamall2.mytransport.sg/ltaodataservice/CarParkAvailabilityv2";
+var myHeaders = new Headers();
+myHeaders.append("AccountKey", "jRHACKiESaGsYNOFNdxSKw==");
 
-api_url = `${apiurl}?AccessKey=${api_key}`;
-console.log(apiurl)
-fetch(apiUrl)
-  .then(response => {
-    // Check if the request was successful (status code 200-299)
-    if (!response.ok) {
-      throw new Error(`HTTP error! Status: ${response.status}`);
-    }
-    return response.json(); // Parse the response as JSON
-  })
-  .then(data => {
-    // Handle the data received from the API
-    console.log(data);
-  })
-  .catch(error => {
-    // Handle any errors that occurred during the fetch
-    console.error('Fetch error:', error);
-  });
+var requestOptions = {
+  method: 'GET',
+  headers: myHeaders,
+  redirect: 'follow'
+};
+
+fetch("http://datamall2.mytransport.sg/ltaodataservice/EstTravelTimes", requestOptions)
+  .then(response => response.text())
+  .then(result => console.log(result))
+  .catch(error => console.log('error', error));
