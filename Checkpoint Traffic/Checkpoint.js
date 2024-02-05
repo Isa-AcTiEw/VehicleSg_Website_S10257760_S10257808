dd8903b30bbb127b25d6e23ae11df37a1899c1e1
Ming Qi <s10257808@connect.np.edu.sg>
2024-02-05 09:08:22 +0800

    checkpoint update

--- Checkpoint Traffic/Checkpoint.js
+++ Checkpoint Traffic/Checkpoint.js
@@ -1,9 +1,34 @@
-var requestOptions = {
-    method: 'GET',
-    redirect: 'follow'
-  };
-  
-  fetch("https://api.getpostman.com/collections/12ece9e1-2abf-4edc-8e34-de66e74114d2", requestOptions)
-    .then(response => response.text())
-    .then(result => console.log(result))
-    .catch(error => console.log('error', error));
+fetch("https://api.data.gov.sg/v1/transport/traffic-images")
+            .then(response => response.json())
+            .then(data => {
+                const woodlandsCP = data.items[0].cameras.find(item => item.camera_id === "2701"); // view from woodlands checkpoint
+                const woodlandsCW = data.items[0].cameras.find(item => item.camera_id === "2702"); // view from woodlands causeway
+                const secondlink = data.items[0].cameras.find(item => item.camera_id === "4703"); // view from second-link
+                const tuasCP = data.items[0].cameras.find(item => item.camera_id === "4713"); // view from tuas checkpoint
+
+
+                displayImage(woodlandsCP, "View from Woodlands Checkpoint");
+                displayImage(woodlandsCW, "View from Woodlands Causeway");
+                displayImage(secondlink, "View from Second Link at Tuas");
+                displayImage(tuasCP, "View from Tuas Checkpoint");
+
+                function displayImage(cameraDetails, altText) {
+                    if (cameraDetails) {
+                        const imageUrl = cameraDetails.image;
+                        const timestamp = new Date(cameraDetails.timestamp).toLocaleString();
+
+                        document.getElementById('result-container').innerHTML += `
+                            <div class="card">
+                                <div class="trf-desc">${altText}</div>
+                                <div class="timestamp">
+                                    <span class="left">${timestamp}</span>
+                                </div>
+                                <img width="400px" src="${imageUrl}" alt="${altText}">
+                            </div>
+                        `;
+                    } else {
+                        console.log(`No image found for camera ID ${cameraDetails.camera_id}`);
+                    }
+                }
+            })
+            .catch(error => console.log('error', error));
